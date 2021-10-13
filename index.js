2c2ff3fa8c0e9ce8be8380defc4788e7e4bc373b
Robert Rynard <rmrynard@gmail.com>
2021-10-13 12:06:25 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var drumArray = document.querySelectorAll(".drum");
+var sounds = {
+    w: "tom-1",
+    a: "tom-2",
+    s: "tom-3",
+    d: "tom-4",
+    j: "snare",
+    k: "crash",
+    l: "kick-bass"
+}
+
+for (i = 0; i < drumArray.length; i++) {
+  drumArray[i].addEventListener("click", function () {
+      
+    
+      
+});
+}
+
+
+
+
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
+
